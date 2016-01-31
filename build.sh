718f178a7fc8ba984bbce22f0fd26a1653dfcd5c
colons <colons@colons.co>
2016-01-31 15:29:29 +0000

    Add a new, bad game.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 convert -resize '1000x1000^' -strip -interlace Plane -gaussian-blur 0.1 -quality 50 games/ymir-orig.jpg games/ymir.jpg
+convert -resize '1000x1000^' -strip -interlace Plane -gaussian-blur 0.1 -quality 50 games/ec-orig.jpg games/ec.jpg
 lessc style.less > style.css
